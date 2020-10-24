cc0b653618c07182eab9be8487437b9dd4c67622
Todd Libby <toddlibby@protonmail.com>
2020-10-23 23:45:20 -0400

    Are you suggesting coconuts migrate?

--- functions/js/index.js
+++ functions/js/index.js
@@ -8,39 +8,44 @@ import VideoDetail from './dist/components/video_detail';
 
 const API_KEY = 'AIzaSyDZLrKAvhr2nMcBFrNwBEqpTiiKHc1q600'; // REPLACE IT WITH YT API KEY
 
-class App extends Component{
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    body: class App extends Component{
 
-  constructor(props){
+      constructor(props){
 
-    super(props);
+        super(props);
 
-    this.state = {
-      videos : [],
-      selectedVideo : null
-    };
+        this.state = {
+          videos : [],
+          selectedVideo : null
+        };
 
-    this.videoSearch('Front End Nerdery');
+        this.videoSearch('Front End Nerdery');
 
-  }
-  videoSearch(term){
-    YTSearch({key : API_KEY, term : term}, (videos) => {
-        this.setState({
-          videos : videos,
-          selectedVideo : videos[0]
-        })
-    });
-  }
-   render(){
-     return(
-       <section>
-        <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect = {selectedVideo => {this.setState({selectedVideo})}}
-          videos={this.state.videos}/>
-       </section>
-     );
-   }
-}
+      }
+      videoSearch(term){
+        YTSearch({key : API_KEY, term : term}, (videos) => {
+            this.setState({
+              videos : videos,
+              selectedVideo : videos[0]
+            })
+        });
+      }
+      render(){
+        return(
+          <section>
+            <SearchBar onSearchTermChange = {term => this.videoSearch(term)}/>
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+              onVideoSelect = {selectedVideo => {this.setState({selectedVideo})}}
+              videos={this.state.videos}/>
+          </section>
+        );
+      }
+    }
 
-ReactDOM.render(<App />,document.querySelector('.container'));
+    ReactDOM.render(<App />,document.querySelector('.container'));
+  });
+};
